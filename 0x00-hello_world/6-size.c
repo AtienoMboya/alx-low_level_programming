5c8b5ef9246d6d3f9eef610d72405da2f4cebe3a
AtienoMboya <sharonmboya894@gmail.com>
2022-03-10 12:41:30 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,10 @@
 /**
  * main-Entry
  *
- * return - Always 0 (success)
- */
+ * Description - To display the sizes of int etc
+ *
+ * Return - Always 0 (success)
+ **/
 int main(void)
 {
 	char charType;
@@ -12,14 +14,11 @@ int main(void)
 	long long int llint;
 	float floatType;
 
-	printf("Size of a char: %ld\n", sizeof(charType));
-	printf("Size of an int: %ld\n", sizeof(intType));
-	printf("Size of a long int: %ld\n", sizeof(lint));
-	printf("Size of a long long int: %ld\n", sizeof(llint));
-	printf("Size of a float: %ld\n", sizeof(floatType));
-	/**
-	 * return - Always 0 (success)
-	 */
+	printf("Size of a char: %ld\n byte(s)", sizeof(charType));
+	printf("Size of an int: %ld\n byte(s)", sizeof(intType));
+	printf("Size of a long int: %ld\n byte(s)", sizeof(lint));
+	printf("Size of a long long int: %ld\n byte(s)", sizeof(llint));
+	printf("Size of a float: %ld\n byte(s)", sizeof(floatType));
 
 	return (0);
 }
