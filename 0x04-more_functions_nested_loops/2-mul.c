4cdb8ea7e75f52bef51271785175fc59582b87f9
AtienoMboya <sharonmboya894@gmail.com>
2022-03-17 13:08:35 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - mutiplies 2 integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,5 @@
 	checks for uppercase character
 1-isdigit.c
 	checks if the given integer is a digit or not
+2-mul.c
+	multiplies 2 integers
