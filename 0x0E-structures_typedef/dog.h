8edca9cdc92c9fd52b59c35c8af19eee3989878e
AtienoMboya <sharonmboya894@gmail.com>
2022-04-11 12:48:35 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure with 3 elements
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
