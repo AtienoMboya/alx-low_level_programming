134936ffd69cfd72d2b247bf1e9ba4d24a22f19a
sharon <sharon@qed.ai>
2023-11-22 21:14:26 +0300

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the given letters
+ *
+ * Description: main prints the given sentence to stdout
+ *
+ * Return: 0 upon completion
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
